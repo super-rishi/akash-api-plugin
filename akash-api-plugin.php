2213d1fb875c39f5185cb2d9a92847911ba207ba
Akash Sharma <5377akash.sharma@gmail.com>
2025-01-25 13:45:34 +0530

    Initialized plugin

---
+++ akash-api-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Akash API Plugin
+ * Plugin URI: https://github.com/super-rishi/akash-api-plugin
+ * Description: A simple plugin that retrieves data from a remote API endpoint, and makes that data accessible/retrievable from an API endpoint on the WordPress site your plugin is installed on. The data will be displayed via a custom block and on an admin WordPress page.
+ * Version: 1.0.0
+ * Requires at least: 6.7
+ * Requires PHP: 7.4
+ * Author: Akash Sharma
+ * Author URI: #
+ * License: GPLv2 or later
+ * Text Domain: akash-api-plugin
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( __('Direct access not allowed.', 'akash-api-plugin') );
+}
+
